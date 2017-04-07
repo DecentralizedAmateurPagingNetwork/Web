1ccf8ae72e7d091b592d91290efe2e44de92973c
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-04-07 10:27:18 +0200

    Replaced " with '

--- static/js/bootstrap-additions.js
+++ static/js/bootstrap-additions.js
@@ -1,6 +1,6 @@
 $().ready(function() {
 	$('.nav a').click(function() {
-		if ($(this).closest("li").children("ul").length === 0) {
+		if ($(this).closest('li').children('ul').length === 0) {
 			$('.navbar-collapse').collapse('hide');
 		}
 	});
