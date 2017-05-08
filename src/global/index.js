cbbbbd04270091effce1dda075ad51dba59b385c
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-05-08 12:04:44 +0200

    Use hsl in stringToColor() (#64)

--- src/global/index.js
+++ src/global/index.js
@@ -102,14 +102,10 @@ const GlobalMethodsPlugin = {
 			let hash = 0;
 			for (let i = 0; i < string.length; i++) {
 				hash = string.charCodeAt(i) + ((hash << 5) - hash);
+				hash = hash & hash;
 			}
 
-			let color = '#';
-			for (let i = 0; i < 3; i++) {
-				let value = (hash >> (i * 8)) & 0xFF;
-				color += ('00' + value.toString(16)).substr(-2);
-			}
-			return color;
+			return 'hsl(' + hash % 360 + ', 100%, 30%)';
 		};
 	}
 };
