1f34347ba794f2253f22376ea8749c9dafef92e9
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-05-08 10:01:53 +0200

    Added stringToColor()

--- src/global/index.js
+++ src/global/index.js
@@ -97,6 +97,20 @@ const GlobalMethodsPlugin = {
 			}
 			return s;
 		};
+
+		Vue.prototype.$helpers.stringToColor = function(string) {
+			let hash = 0;
+			for (let i = 0; i < string.length; i++) {
+				hash = string.charCodeAt(i) + ((hash << 5) - hash);
+			}
+
+			let color = '#';
+			for (let i = 0; i < 3; i++) {
+				let value = (hash >> (i * 8)) & 0xFF;
+				color += ('00' + value.toString(16)).substr(-2);
+			}
+			return color;
+		};
 	}
 };
 
