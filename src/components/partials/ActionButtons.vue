c920235ec247dae996d3413221d32ba1c8b1d67d
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-05-09 14:51:09 +0200

    Moved mail-button to end of list

--- src/components/partials/ActionButtons.vue
+++ src/components/partials/ActionButtons.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		<button v-if="mailAction" @click="mailAction(element)" class="btn btn-default btn-xs"><i class="fa fa-envelope" title="Send Email"></i></button>
 		<button v-if="editAction" @click="editAction(element)" class="btn btn-info btn-xs"><i class="fa fa-pencil" title="Edit"></i></button>
 		<button v-if="deleteAction" @click="deleteAction(element)" class="btn btn-primary btn-xs"><i class="fa fa-trash" title="Delete"></i></button>
+		<button v-if="mailAction" @click="mailAction(element)" class="btn btn-default btn-xs"><i class="fa fa-envelope" title="Send Email"></i></button>
 	</div>
 </template>
 
