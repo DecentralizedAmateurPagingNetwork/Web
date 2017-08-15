f2bb35d5350135250a36dc8ea0f0dff991af9e44
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2017-08-15 13:20:41 +0200

    Added version and commit to console-output

--- src/main.js
+++ src/main.js
@@ -87,6 +87,12 @@ Vue.http.options.timeout = 30000;
 Vue.http.headers.common['Authorization'] = 'Basic ' + store.getters.user.auth;
 delete Vue.http.headers.common['Content-Type'];
 
+// output version and links onto console
+const pkg = require('../package.json');
+console.log('%c DAPNET Web v' + pkg.version + ' ', 'background: #112a2d; color: #bada55; font-size: large;');
+console.log('Latest commit:    https://github.com/DecentralizedAmateurPagingNetwork/Web/commit/' + process.env.GIT.COMMITHASH);
+console.log('More information: https://github.com/DecentralizedAmateurPagingNetwork/Web');
+
 // init Vue-instance
 /* eslint-disable no-new */
 new Vue({
