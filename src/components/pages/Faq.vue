153a82df063d985d605a26561f6229d8e5885aec
Ralf Wilke DH3WR <dh3wr@users.noreply.github.com>
2017-11-20 13:51:25 +0100

    Added Info about AlphaPOC RIC assignment

--- src/components/pages/Faq.vue
+++ src/components/pages/Faq.vue
@@ -49,7 +49,10 @@
 				<h2>I do not receive the rubrics on my skyper.</h2>
 				<p>Please activate your skyper on <a href="http://www.hampager.de/#/rubrics" target="_blank">http://www.hampager.de/#/rubrics</a> using the "Activate Rubrics" function. You have to provide your pager ID (RIC).</p>
 
-				<h2>I do not have skyper but a AlphaPOC receiver. How can I receive the rubric's content.</h2>
+				<h2>I do not have skyper but a AlphaPOC receiver. How can I receive the rubric's content?</h2>
+				<p>It is now common sense to take your DMR ID an substract 60000 from it. So DMR ID 2621234 will result in POCSAG RIC 2021234. If you don't have a DMR ID assigned to you, please contact us and we will assign you a sepearte RIC.</p>
+
+				<h2>What RIC should I assign to my AlphaPOC receiver, as there is no pre-defined RIC on these devices?</h2>
 				<p>New rubric content is send both in skyper format and to RIC number (1000 + <rubric-number>). So Rubric number 31 is RIC 1031. Add this RIC to your receiver.</p>
 
 				<h2>I want to add a rubric to the network.</h2>
