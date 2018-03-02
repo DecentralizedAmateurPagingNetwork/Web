0a7b4712c7c52c9d77ca7fefcb073cc4065f282f
Ralf Wilke <dh3wr@darc.de>
2018-03-02 12:17:37 +0100

    Reuse of navigation definition "all transmitters"

--- src/components/transmitter/Overview.vue
+++ src/components/transmitter/Overview.vue
@@ -10,7 +10,7 @@
 
 		<div class="row">
 			<div class="col-lg-9">
-				<h2>{{ $t('transmitter.overview.table.title') }}
+				<h2>{{ $t('navigation.transmitters.all') }}
 					<i class="fa fa-refresh fa-fw" :class="{ 'fa-spin': running }" @click="loadData"></i>
 				</h2>
 
