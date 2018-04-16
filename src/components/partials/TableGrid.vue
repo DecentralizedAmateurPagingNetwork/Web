dc9c49603cf3ab0a4c93952e71c30a47cfa43e91
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2018-04-16 15:54:09 +0200

    TableGrid: expanded column-size to 4 (#159)

--- src/components/partials/TableGrid.vue
+++ src/components/partials/TableGrid.vue
@@ -10,7 +10,7 @@
 				</div>
 			</div>
 
-			<div class="col-lg-3 col-lg-push-5">
+			<div class="col-lg-4 col-lg-push-4">
 				<div id="paginationHolder">
 					<button class="btn btn-default btn-xs" @click="movePages(-1)"><i class="fa fa-arrow-left"></i></button>
 					&nbsp;{{ $t('table.pagination.page') }} <input v-model.number="gotoPage" type="number" min="1" :max="totalPages"> {{ $t('table.pagination.of') }} {{ totalPages }}&nbsp;
