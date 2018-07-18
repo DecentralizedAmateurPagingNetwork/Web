0ed478bee1aaf44ecff8931d0767690c0d73a3f6
Marvin Menzerath <menzerath@ihf.rwth-aachen.de>
2018-07-18 10:30:03 +0200

    Added node monitoring

--- src/components/node/Monitoring.vue
+++ src/components/node/Monitoring.vue
@@ -3,28 +3,59 @@
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="page-header">
-					<h1>{{ $t('pagetitle.node.monitoring') }}</h1>
+					<h1>{{ $t('pagetitle.node.monitoring') }}: {{ $route.params.id }}</h1>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" v-if="ws.readyState === 1">
+			<div class="col-lg-3">
+				<h2>General Status</h2>
+				<ul class="list-group">
+					<li class="list-group-item"><b>Healthy</b>
+						<span class="badge" :class="[monitoringData.good_health ? 'badge-success' : 'badge-danger']">{{ monitoringData.good_health | trueFalse }}</span>
+					</li>
+				</ul>
+			</div>
+			<div class="col-lg-3">
+				<h2>Connections</h2>
+				<ul class="list-group">
+					<li class="list-group-item"><b>Transmitters</b><span class="badge">{{ monitoringData.connections.transmitters }}</span></li>
+					<li class="list-group-item"><b>Third Party</b><span class="badge">{{ monitoringData.connections.third_party }}</span></li>
+					<li class="list-group-item"><b>Websocket</b><span class="badge">{{ monitoringData.connections.websocket }}</span></li>
+				</ul>
+			</div>
+			<div class="col-lg-6">
+				<h2>System</h2>
+				<ul class="list-group">
+					<li class="list-group-item"><b>Free Disk Space</b><span class="badge">{{ monitoringData.system.free_disk_space_mb }} MB</span></li>
+					<li class="list-group-item"><b>CPU Utilization</b><span class="badge">{{ monitoringData.system.cpu_utilization }} %</span></li>
+					<li class="list-group-item"><b>Hamcloud?</b><span class="badge">{{ monitoringData.system.is_hamcloud | trueFalse }}</span></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row" v-if="ws.readyState === 1">
+			<div class="col-lg-12">
+				<h2>Services</h2>
+				<div class="row">
+					<div class="col-lg-2" v-for="(sData, sName) in monitoringData.microservices" :key="sName">
+						<h3><code>{{ sName }}</code></h3>
+						<ul class="list-group">
+							<li class="list-group-item"><b>Healthy</b>
+								<span class="badge" :class="[sData.ok ? 'badge-success' : 'badge-danger']">{{ sData.ok | trueFalse }}</span>
+							</li>
+							<li class="list-group-item" v-if="sData.version"><b>Version</b><span class="badge">{{ sData.version }}</span></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
 
 		<div class="row">
 			<div class="col-lg-12">
-				<div v-if="ws.readyState === 0" class="progress progress-striped active">
-					<div class="progress-bar" style="width: 0%"></div>
-				</div>
-				<div v-if="ws.readyState === 1" class="progress progress-striped active">
-					<div class="progress-bar progress-bar-success" style="width: 100%"></div>
-				</div>
-				<div v-if="ws.readyState === 2 || ws.readyState === 3" class="progress progress-striped active">
-					<div class="progress-bar" style="width: 100%"></div>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-lg-12">
-				<pre><code>{{ message }}<br></code></pre>
+				<h2>Raw Data</h2>
+				<pre><code>{{ monitoringData }}<br></code></pre>
 			</div>
 		</div>
 	</div>
@@ -39,19 +70,52 @@
 			this.ws.addEventListener('message', e => {
 				let data = JSON.parse(e.data);
 
-				if (data.type !== 'node') return;
-				this.message = data;
+				// this transmitter only
+				if (data.type !== 'node' || data.name !== this.$route.params.id) return;
+
+				if (!this.monitoringData) {
+					// save initial copy of data
+					this.monitoringData = data;
+				} else {
+					// overwrite with new chunk of data
+					this.monitoringData = Object.assign(this.monitoringData, data);
+				}
 			});
 		},
 		data() {
 			return {
 				ws: false,
-				message: ''
+				monitoringData: false
 			};
+		},
+		filters: {
+			trueFalse: function(value) {
+				if (value) return 'yes';
+				return 'no';
+			}
 		}
 	};
 </script>
 
 <style scoped>
+	h3 > code {
+		font-size: 80%;
+	}
 
+	.badge {
+		background-color: #013c51;
+	}
+
+	.badge-success {
+		background-color: #469408;
+	}
+
+	.badge-danger {
+		background-color: #d9230f;
+	}
+
+	.label-timeslot {
+		font-size: small;
+		margin-right: 2px;
+	}
 </style>
